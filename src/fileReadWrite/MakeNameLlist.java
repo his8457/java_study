fb2af11df00e638c8f554207fd073c29a77eceef
ishwang <ishwang@DESKTOP-80U9V7A>
2021-05-12 00:18:58 +0900

    텍스트 파일 출력

---
+++ src/fileReadWrite/MakeNameLlist.java
@@ -0,0 +1,60 @@
+package fileReadWrite;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MakeNameLlist {
+
+	public static void main(String[] args) {
+		//1. 파일로 내보낼 사람이름 램덤 100명
+		
+		//char a = 0xAC00; // 0xAC00가 ~ 0xD7A3힣
+		char[] lastNames = {'김','이','박','최','고','황','오','정','조','백'};
+		
+		StringBuffer strNames = new StringBuffer();
+		char lastName = ' ';
+		char firstName = ' ';
+		String fullName = "";
+		
+		for(int i = 0; i < 10; i++) {
+			lastName = lastNames[i];
+			for(int j = 0; j < 10; j++) {
+				fullName = Character.toString(lastName);
+				fullName += "*";
+				firstName = (char)((Math.random() * 11172) + 0xAC00);
+				
+				fullName += Character.toString(firstName);
+				//System.out.println(j + " : " + fullName);
+				
+				strNames.append(fullName);
+				
+				if(i != 9 || j != 9) {
+					strNames.append("\n");
+				}
+			}
+		}
+		
+		File file = new File("src/fileReadWrite/test.txt");
+		System.out.println(file.getAbsolutePath());
+		
+		FileWriter fw = null;
+		try {
+			//fw = new FileWriter("hundredNames.txt");
+			fw = new FileWriter(file);
+			fw.write(strNames.toString());
+			fw.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
